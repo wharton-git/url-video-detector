d189a2f274a5e4db01dadc50aa19ff7c0732e8d3
Xeon <whartonaldrick@gmail.com>
2025-05-12 02:46:23 +0300

    Refactoring : move Css Style into style.css file
    Fixes #3

--- content.js
+++ content.js
@@ -5,63 +5,19 @@ const leftContainer = document.createElement("div");
 const rightContainer = document.createElement("div");
 const txt = document.createElement("div");
 const urlText = document.createElement("div");
-const txtS = txt.style;
-const rightContainerS = rightContainer.style;
-const leftContainerS = leftContainer.style;
-const closeBtnS = closeBtn.style;
-const notifierS = notifier.style;
-const btnDownS = btnDown.style;
+const link = document.createElement("link");
 
-//Elements styles
-notifierS.position = "fixed";
-notifierS.display = "flex";
-notifierS.flexDirection = "row";
-notifierS.alignItems = "center";
-notifierS.bottom = "20px";
-notifierS.right = "20px";
-notifierS.color = "white";
-notifierS.zIndex = "9999";
-notifierS.borderRadius = "5px";
-notifierS.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
-notifierS.backgroundColor = "rgba(0, 0, 0, 0.8)";
-notifierS.overflow = "hidden";
+//Add CSS to the page
+link.rel = "stylesheet";
+link.href = chrome.runtime.getURL("notifier.css");
+document.head.appendChild(link);
 
-closeBtnS.backgroundColor = "";
-closeBtnS.color = "white";
-closeBtnS.padding = "5px";
-closeBtnS.width = "50%";
-closeBtnS.height = "100%";
-closeBtnS.textAlign = "center";
-closeBtnS.cursor = "pointer";
-closeBtnS.display = "flex";
-closeBtnS.alignItems = "center";
-closeBtnS.justifyContent = "center";
-closeBtnS.borderRadius = "50%";
-
-btnDownS.backgroundColor = "#44B852";
-btnDownS.color = "white";
-btnDownS.padding = "5px";
-btnDownS.width = "50%";
-btnDownS.height = "100%";
-btnDownS.textAlign = "center";
-btnDownS.cursor = "pointer";
-btnDownS.display = "flex";
-btnDownS.alignItems = "center";
-btnDownS.justifyContent = "center";
-
-leftContainerS.width = "80%";
-leftContainerS.display = "flex";
-leftContainerS.flexDirection = "column";
-leftContainerS.alignItems = "flex-start";
-leftContainerS.padding = "5px";
-leftContainerS.overflow = "hidden";
-leftContainerS.textOverflow = "ellipsis";
-leftContainerS.whiteSpace = "nowrap";
-
-rightContainerS.width = "20%";
-rightContainerS.display = "flex";
-rightContainerS.height = "100%";
-
+//Add classes to the elements
+notifier.className = "notifier";
+closeBtn.className = "close-btn";
+btnDown.className = "download-btn";
+leftContainer.className = "left-container";
+rightContainer.className = "right-container";
 
 //Elements DOM events
 closeBtn.addEventListener("click", () => {
@@ -122,3 +78,4 @@ const sendParameters = async () => {
         })
     });
 };
+
